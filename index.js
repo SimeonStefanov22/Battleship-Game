8c26ee2922e5351939ce9afdcb0dd1a65fddb863
SimeonStefanov22 <simeonstefanov1@gmail.com>
2019-09-15 14:41:09 +0300

    Upgrade

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function main() {
+(function createGameBoardAndShips() {
     const rows = 10;
     const cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
     class Ship {
@@ -13,23 +13,19 @@
     battleShip.length = 5;
     firstDestroyer.length = 4;
     secondDestroyer.length = 4;
-    let gameBoardContainer = document.getElementById("gameBoard");
-    let numbersTable = document.getElementById("numbersBoard");
-    let lettersTable = document.getElementById("letters");
-    let emptyString = document.createElement("div");
+    const gameBoardContainer = document.getElementById("gameBoard");
+    const numbersTable = document.getElementById("numbersBoard");
+    const lettersTable = document.getElementById("letters");
+    const emptyString = document.createElement("div");
     numbersTable.appendChild(emptyString);
     let square;
-    let randomNums = randomNumber();
-    let startPointBattleShip = randomNums[0];
-    let startPointFirstDestroyer = randomNums[1];
-    let startPointSecondDestroyer = randomNums[2];
+    const randomNums = randomNumber();
+    const startPointBattleShip = randomNums[0];
+    const startPointFirstDestroyer = randomNums[1];
+    const startPointSecondDestroyer = randomNums[2];
     let pointHorizontalBattleShip = startPointBattleShip;
     let pointVerticalFirstDestroyer = startPointFirstDestroyer;
     let pointVerticalSecondDestroyer = startPointSecondDestroyer;
-
-    console.log(startPointBattleShip);
-    console.log(startPointFirstDestroyer);
-    console.log(startPointSecondDestroyer);
 
     for (let i = 1; i <= cols.length; i++) {
         let number = document.createElement("div");
@@ -68,14 +64,12 @@
             gameBoardContainer.appendChild(square);
         }
     }
-    //console.log(gameBoardContainer);
     document.getElementById("shootInput").value = '';
-
 })()
 
 function randomNumber() {
-    let min = 1;
-    let max = 5;
+    const min = 1;
+    const max = 5;
     let randomNumber = 0;
     let randNums = [];
     for (let n = 0; n < 3; n++) {
@@ -87,7 +81,6 @@ function randomNumber() {
     //Created different random numbers
     randNums[1]++;
     randNums[2]+= 2;
-
     return randNums;
 }
 
@@ -95,13 +88,13 @@ let  countShoot = 0;
 
 function shot(){
     let coordinates = document.getElementById("shootInput");
-    let gameBoardContainer = document.getElementsByClassName("separateSquare");
-    let partOfShip = "ship";
+    const gameBoardContainer = document.getElementsByClassName("separateSquare");
+    const partOfShip = "ship";
     let  messageAfterTheShot = document.getElementById("message");
     for (let separateSquare of gameBoardContainer) {
         let ship = separateSquare.getAttribute("name");
         let shipId = separateSquare.getAttribute("id");
-        let shipName = separateSquare.getAttribute("data");
+        separateSquare.getAttribute("data");
         if (coordinates.value.toUpperCase() === shipId && partOfShip === ship) {
             separateSquare.removeAttribute("name");
             separateSquare.removeAttribute("id");
@@ -122,8 +115,6 @@ function shot(){
 function message() {
     let gameBoardContainer = document.getElementsByClassName("separateSquare");
     let messageSunk = document.getElementById("messageSunk");
-
-
     let countShips = 0;
     let counterBattleShip = 0;
     let counterFirstDestroyer = 0;
@@ -148,13 +139,13 @@ function message() {
     }
     if (counterBattleShip === 0){
 
-        messageSunk.innerHTML = "*** Battle ship sunk ***";
+        messageSunk.innerHTML = "*** Battle ship is sunk ***";
     }
     if (counterFirstDestroyer === 0 ) {
-        messageSunk.innerHTML = "*** First Destroyer ship sunk ***";
+        messageSunk.innerHTML = "*** First Destroyer is ship sunk ***";
     }
     if (counterSecondDestroyer === 0) {
-        messageSunk.innerHTML = "*** Second Destroyer ship sunk ***";
+        messageSunk.innerHTML = "*** Second Destroyer is ship sunk ***";
     }
     if (countShips === 0) {
         messageSunk.innerHTML = `Well done! You completed the game in ${countShoot} shots`;
